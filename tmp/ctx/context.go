cb7081344fb2f4b09c8d650b93d586ad9baf022b
shoichiimamura <immrshich@gmail.com>
2020-10-18 21:58:42 +0900

    :sparkles: Cancel a nested context

--- tmp/ctx/context.go
+++ tmp/ctx/context.go
@@ -4,25 +4,47 @@ import (
 	"context"
 	"fmt"
 	"sync"
-	"time"
 )
 
-func main() {
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
-	defer cancel()
+//func main() {
+//	var wg sync.WaitGroup
+//	wg.Add(1)
+//
+//	ctx, cancel := context.WithCancel(context.Background())
+//
+//	go func(ctx context.Context) {
+//		select {
+//		case <-ctx.Done():
+//			fmt.Println("----done----")
+//			wg.Done()
+//			return
+//		}
+//	}(ctx)
+//
+//	cancel()
+//	wg.Wait()
+//}
 
+func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	go func(c context.Context) {
-		select {
-		case <-c.Done():
-			fmt.Println(c.Err())
-			wg.Done()
-			return
-		}
-	}(ctx)
+	c1, can1 := context.WithCancel(context.Background())
 
+	go func(ctx context.Context) {
+		c2, _ := context.WithCancel(ctx)
+
+		go func(ctx context.Context) {
+			c3, _ := context.WithCancel(ctx)
+			select {
+			case <-c3.Done():
+				fmt.Println("----done----")
+				wg.Done()
+				return
+			}
+		}(c2)
+	}(c1)
+
+	can1()
 	wg.Wait()
 }
