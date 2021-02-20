e17b5f60ec1de25f2c082647ab14f052e604466a
shoichiimamura <immrshich@gmail.com>
2021-02-21 02:00:55 +0900

    :sparkles: Send a request

--- tmp/retry/retry.go
+++ tmp/retry/retry.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
+	res, err := http.Get("http://www.google.com/robots.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := res.Body.Close(); err != nil {
+		log.Fatal(err)
+	}
+	t := http.DefaultTransport
+	fmt.Printf("%+v", t)
 }
