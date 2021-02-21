ffe430298ab76ce042bfd0b3c9f66d498aa9b502
shoichiimamura <immrshich@gmail.com>
2021-02-22 02:14:33 +0900

    :sparkles: Rewind a body

--- tmp/retry/retry.go
+++ tmp/retry/retry.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"errors"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -18,9 +21,20 @@ type Client struct {
 }
 
 func (c *Client) Do(req *http.Request) (*http.Response, error) {
+	buf, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+	if err := req.Body.Close(); err != nil {
+		return nil, err
+	}
+	bodyReader := func() io.ReadCloser {
+		return ioutil.NopCloser(bytes.NewReader(buf))
+	}
 	var attemptNum int
 	for {
 		attemptNum++
+		req.Body = bodyReader() // TODO: net/http.Transportのrewindの実装を確認する
 		res, err := c.HTTPClient.Do(req)
 		shouldRetry := c.CheckRetry(res, err)
 		if !shouldRetry {
