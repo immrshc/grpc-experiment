6e49a47dbbb8b3d5885dbd6d3089d63311366197
shoichiimamura <immrshich@gmail.com>
2021-02-22 23:00:06 +0900

    :sparkles: Change the logic to rewind body

--- tmp/retry/retry.go
+++ tmp/retry/retry.go
@@ -19,21 +19,30 @@ type Client struct {
 	Backoff    Backoff
 }
 
+func NewClient() *Client {
+	return &Client{
+		RetryMax:   3,
+		HTTPClient: http.DefaultClient,
+		CheckRetry: func(res *http.Response, err error) bool {
+			if err == nil {
+				return false
+			}
+			return res.StatusCode >= http.StatusInternalServerError
+		},
+		Backoff: func(attemptNum int) time.Duration {
+			return time.Second * time.Duration(attemptNum)
+		},
+	}
+}
+
 func (c *Client) Do(req *http.Request) (*http.Response, error) {
-	buf, err := io.ReadAll(req.Body)
-	if err != nil {
-		return nil, err
-	}
-	if err := req.Body.Close(); err != nil {
-		return nil, err
-	}
-	bodyReader := func() io.ReadCloser {
-		return io.NopCloser(bytes.NewReader(buf))
-	}
 	var attemptNum int
 	for {
 		attemptNum++
-		req.Body = bodyReader() // TODO: net/http.Transportのrewindの実装を確認する
+		req, err := rewindBody(req)
+		if err != nil {
+			return nil, err
+		}
 		res, err := c.HTTPClient.Do(req)
 		shouldRetry := c.CheckRetry(res, err)
 		if !shouldRetry {
@@ -47,21 +56,34 @@ func (c *Client) Do(req *http.Request) (*http.Response, error) {
 	}
 }
 
-func main() {
-	client := Client{
-		RetryMax:   3,
-		HTTPClient: http.DefaultClient,
-		CheckRetry: func(res *http.Response, err error) bool {
-			if err == nil {
-				return false
-			}
-			return res.StatusCode >= http.StatusInternalServerError
-		},
-		Backoff: func(attemptNum int) time.Duration {
-			return time.Second * time.Duration(attemptNum)
-		},
+func rewindBody(req *http.Request) (*http.Request, error) {
+	if req.Body == nil || req.Body == http.NoBody {
+		return req, nil
 	}
-	req, err := http.NewRequest("GET", "http://www.google.com/robots.txt", nil)
+	if req.GetBody == nil {
+		buf, err := io.ReadAll(req.Body)
+		if err != nil {
+			return nil, err
+		}
+		req.GetBody = func() (io.ReadCloser, error) {
+			return io.NopCloser(bytes.NewReader(buf)), nil
+		}
+	}
+	if err := req.Body.Close(); err != nil {
+		return nil, err
+	}
+	body, err := req.GetBody()
+	if err != nil {
+		return nil, err
+	}
+	newReq := *req
+	newReq.Body = body
+	return &newReq, nil
+}
+
+func main() {
+	client := NewClient()
+	req, err := http.NewRequest("POST", "http://www.google.com/robots.txt", bytes.NewBuffer([]byte("\"{\"x\":1,\"y\":2}\"")))
 	if err != nil {
 		log.Fatal(err)
 	}
